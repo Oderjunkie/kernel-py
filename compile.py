f7fc44165ec089d88903c759c6b2e386bd2a4ea0
Oderjunkie <58880677+Oderjunkie@users.noreply.github.com>
2021-03-24 19:58:58 +0300

    allow more than 4 arguments passed to functions

--- compile.py
+++ compile.py
@@ -4,7 +4,7 @@ import ast
 constcount = 0
 data = []
 code = []
-registers = ['rcx', 'rdx', 'r8', 'r9']
+registers = ['rcx', 'rdx', 'r8', 'r9'] + ['']*21
 def compiler(name):
     global constcount
     tree = ast.parse(open(name, 'r').read(), name, type_comments=True)
@@ -25,24 +25,47 @@ def compiler(name):
             return ''.join(result)
         def parse_Expr(part):
             global constcount
+            global code
             if type(part.value)==ast.Call:
-                if len(part.value.args)>4:
-                    raise Exception('No more than 4 arguments!')
+                subq = 0
+                if len(part.value.args)>len(registers):
+                    raise Exception('No more than {} arguments per function!'.format(len(registers)))
                 if part.value.func.value.id=='stdio':
-                    for arg, reg in zip(part.value.args, registers):
+                    #print(list(zip(part.value.args)))
+                    temp = []
+                    for num, (arg, reg) in enumerate(zip(part.value.args, registers)):
+                        if reg=='':
+                            reg = subq+32
+                            subq += 8
                         if type(arg.value)==str:
                             data.append('.LC{}: .asciz "{}"'.format(constcount, escape(arg.value)))
-                            code.append('leaq .LC{}(%rip), %{}'.format(constcount, reg))
+                            if num<4:
+                                temp.append('leaq .LC{}(%rip), %{}'.format(constcount, reg))
+                            else:
+                                temp.append('leaq .LC{}(%rip), %rax'.format(constcount))
+                                temp.append('movq %rax, {}(%rsp)'.format(reg))
                             constcount += 1
                         if type(arg.value)==float:
-                            code.append('leaq ${}, %{}'.format(int(arg.value), reg))
+                            if num<4:
+                                temp.append('movq ${}, %{}'.format(int(arg.value), reg))
+                            else:
+                                temp.append('movq ${}, %rax'.format(int(arg.value)))
+                                temp.append('movq %rax, {}(%rsp)'.format(reg))
                         if type(arg.value)==int:
-                            code.append('leaq ${}, %{}'.format(arg.value, reg))
+                            if num<4:
+                                temp.append('movq ${}, %{}'.format(arg.value, reg))
+                            else:
+                                temp.append('movq ${}, %rax'.format(arg.value))
+                                temp.append('movq %rax, {}(%rsp)'.format(reg))
+                    if subq>0: code.append('subq ${}, %rsp'.format(subq))
+                    code += temp
                     code.append('call {}'.format(part.value.func.attr))
+                    if subq>0: code.append('addq ${}, %rsp'.format(subq))
         if type(part)==ast.Module: return parse_Module(part)
         if type(part)==ast.Import: return parse_Import(part)
         if type(part)==ast.ImportFrom: return parse_ImportFrom(part)
         if type(part)==ast.Expr: return parse_Expr(part)
+        print(ast.dump(part))
         return True
     compiler_int(tree)
     codetext = '\n'.join(code)
@@ -56,8 +79,6 @@ main:
 pushq %rbp
 movq %rsp, %rbp
 subq $32, %rsp
-movl %ecx, 16(%rbp)
-movq %rdx, 24(%rbp)
 call __main
 {}
 movl $0, %eax
